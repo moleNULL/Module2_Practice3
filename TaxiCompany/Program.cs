1ef8d61fc6f214856fa27ec89ef51e65c0d60102
moleNULL <94998782+moleNULL@users.noreply.github.com>
2022-11-17 22:38:07 +0200

    Add try-catch

--- TaxiCompany/Program.cs
+++ TaxiCompany/Program.cs
@@ -36,7 +36,14 @@ namespace TaxiCompany
     {
         public static void Main(string[] args)
         {
-            Starter.Run();
+            try
+            {
+                Starter.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception! {ex.Message}");
+            }
 
             Console.Write("\nPress any key to continue . . .");
             Console.ReadKey();
--- TaxiCompany/Starter.cs
+++ TaxiCompany/Starter.cs
@@ -26,7 +26,7 @@
         public static Car[] BuyNewCars()
         {
             BMW bmw = new BMW(new BmwOnDiesel(model: "320", dieselConsumption: 7, carPrice: 3999, year: 2000));
-            var mitsubishi = new PetrolCar(brand: "Mitsubishi", model: "Lancer", country: "Japan", PetrolType.AI95, petrolConsumption: 7.4, carPrice: 6000, year: 2008);
+            var mitsubishi = new PetrolCar(brand: "Mitsubishi", model: "Lancer", country: "Japan", PetrolType.Test, petrolConsumption: 7.4, carPrice: 6000, year: 2008);
             Renault renault = new Renault(new RenaultOnDiesel(model: "Laguna", dieselConsumption: 6.5, carPrice: 4800, year: 2008));
             var ford = new GasCar(brand: "Ford", model: "C-Max 2", country: "USA", GasType.Methane, gasConsumption: 8.4, carPrice: 3900, year: 2006);
 
